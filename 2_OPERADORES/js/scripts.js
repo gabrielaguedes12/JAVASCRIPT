894f4658b337f7e30f49340829957a805e2c3183
gabrielaguedes12 <git@github.com:gabrielaguedes12/curso-front-end.git>
2025-04-05 22:45:45 -0300

    progresso

--- 2_OPERADORES/js/scripts.js
+++ 2_OPERADORES/js/scripts.js
@@ -1,7 +1,35 @@
-// 1-Number
-console.log(typeof 2);
-console.log(typeof 5.14);
-console.log(typeof -127);
+// // 1-Number
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -127);
 
-//2. ops. aritmeticas
-console.log()
+// //2. ops. aritmeticas
+// console.log(2+4);
+// console.log(6-1);
+// console.log(6/2);
+// console.log(6*6);
+
+// // 3.typeof
+// console.log(typeof Infinity);
+// console.log(typeof -Infinity);
+
+// console.log(12*"jshdjs");
+
+// console.log(typeof NaN);
+
+// //texto
+// console.log("Teste texto dupla");
+// console.log('Teste texto simples');
+// console.log(`Teste texto crase`);
+
+// console.log(typeof "Teste texto dupla");
+
+// //simbolos especiais em string
+// console.log("testando a \nquebra de linha");
+// console.log("espaçamento de \t tab");
+
+//6 - concatenação
+console.log("oi, " + " tudo" +" bem?");
+
+//7- interpolação
+console.log(`A soma de  2 + 2 é: ${2 + 2}`);
